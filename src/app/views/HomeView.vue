6d44389306e2c3392f54fd460562d8de587beb4f
Oscar-Raygoza <oscar.eduardo.raygoza@gmail.com>
2024-12-18 12:50:39 -0600

    feat: install taildwind css ✨

--- src/app/views/HomeView.vue
+++ src/app/views/HomeView.vue
@@ -10,6 +10,7 @@ onMounted(async () => {
 </script>
 
 <template>
+  <h1 className="text-3xl font-bold underline !uppercase">Hello world!</h1>
   <main>
     <img :src="cardsStore?.cards[0]?.image" width="300px" style="border-radius: 5px" />
   </main>
--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwindcss from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   define: {
     'process.env': {
-      POKEMONTCG_API_KEY: '80459b3c-0154-4045-9ff2-19381dbed141'
-    }
-  }
+      POKEMONTCG_API_KEY: '80459b3c-0154-4045-9ff2-19381dbed141',
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
