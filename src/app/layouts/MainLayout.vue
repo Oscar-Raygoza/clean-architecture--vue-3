3b1c9d12b50243d5d1e4192cb1ae2d8c8b4a23de
Oscar-Raygoza <oscar.eduardo.raygoza@gmail.com>
2024-12-30 16:35:10 -0600

    fix: remove div on main layout

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -3,13 +3,11 @@ import SimpleNavbar from '@/app/components/Shared/Layout/Navbar/SimpleNavbar.vue
 import Footer from '@/app/components/Shared/Layout/Footer/Footer.vue'
 </script>
 <template>
-  <div class="layout-container">
-    <SimpleNavbar />
-    <main>
-      <slot>
-        <router-view />
-      </slot>
-    </main>
-    <Footer />
-  </div>
+  <SimpleNavbar />
+  <main>
+    <slot>
+      <router-view />
+    </slot>
+  </main>
+  <Footer />
 </template>
