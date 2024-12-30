9cd3b0d45d1f820e1ca369eb7ca0d43d39e2fb57
Oscar-Raygoza <oscar.eduardo.raygoza@gmail.com>
2024-12-30 16:35:58 -0600

    refactor(build):change to loadenv value on tcg api key

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,21 +8,26 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  define: {
-    'process.env': {
-      POKEMONTCG_API_KEY: '80459b3c-0154-4045-9ff2-19381dbed141',
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+    define: {
+      'process.env': {
+        POKEMONTCG_API_KEY: env.VITE_POKEMONTCG_API_KEY,
+      },
     },
-  },
+  }
 })
