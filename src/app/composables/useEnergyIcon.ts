a8ac6f2b58b23c81e7af58ee468448303bf00c29
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2025-04-01 17:02:07 -0600

    fix: import icons

--- src/app/composables/useEnergyIcon.ts
+++ src/app/composables/useEnergyIcon.ts
@@ -1,16 +1,28 @@
+import Colorless from '@/app/assets/static/energy/colorless.png'
+import Darkness from '@/app/assets/static/energy/darkness.png'
+import Dragon from '@/app/assets/static/energy/dragon.png'
+import Fairy from '@/app/assets/static/energy/fairy.png'
+import Fighting from '@/app/assets/static/energy/fighting.png'
+import Fire from '@/app/assets/static/energy/fire.png'
+import Grass from '@/app/assets/static/energy/grass.png'
+import Lightning from '@/app/assets/static/energy/lightning.png'
+import Metal from '@/app/assets/static/energy/metal.png'
+import Psychic from '@/app/assets/static/energy/psychic.png'
+import Water from '@/app/assets/static/energy/water.png'
+
 export const useEnergyIcon = (energyType: string) => {
   const energyIcons = {
-    Colorless: '/src/app/assets/static/energy/colorless.png',
-    Darkness: '/src/app/assets/static/energy/darkness.png',
-    Dragon: '/src/app/assets/static/energy/dragon.png',
-    Fairy: '/src/app/assets/static/energy/fairy.png',
-    Fighting: '/src/app/assets/static/energy/fighting.png',
-    Fire: '/src/app/assets/static/energy/fire.png',
-    Grass: '/src/app/assets/static/energy/grass.png',
-    Lightning: '/src/app/assets/static/energy/lightning.png',
-    Metal: '/src/app/assets/static/energy/metal.png',
-    Psychic: '/src/app/assets/static/energy/psychic.png',
-    Water: '/src/app/assets/static/energy/water.png',
+    Colorless,
+    Darkness,
+    Dragon,
+    Fairy,
+    Fighting,
+    Fire,
+    Grass,
+    Lightning,
+    Metal,
+    Psychic,
+    Water,
   }
 
   return energyIcons[energyType as keyof typeof energyIcons] || energyIcons.Colorless
